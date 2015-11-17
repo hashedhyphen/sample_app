d499f62f0e89723238fb6ce18c8ee644af17b677
hashedhyphen <hashedhyphen@gmail.com>
2015-11-17 23:34:21 +0900

    Add user microposts

---
+++ temp.rb
@@ -0,0 +1,9 @@
+require "tempfile"
+require "tmpdir"
+
+t = Tempfile.open(['hoge', 'bar'])
+puts Dir.tmpdir
+
+while true do
+  #
+end
---
+++ test/integration/users_profile_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class UsersShowTest < ActionDispatch::IntegrationTest
+  include ApplicationHelper
+
+  def setup
+    @user = users(:michael)
+    @activated     = users(:michael)
+    @non_activated = users(:non_activated)
+  end
+
+  test "show an activated user only" do
+    # Activated user
+    get user_path(@activated)
+    assert_template 'users/show'
+    # Non activated user
+    get user_path(@non_activated)
+    assert_redirected_to root_url
+  end
+
+  test "profile display" do
+    get user_path(@user)
+    assert_template 'users/show'
+    assert_select 'title', full_title(@user.name)
+    assert_select 'h1', text: @user.name
+    assert_select 'h1>img.gravatar'
+    assert_match  @user.microposts.count.to_s, response.body
+    assert_select 'div.pagination'
+    @user.microposts.paginate(page: 1).each do |micropost|
+      assert_match micropost.content, response.body
+    end
+  end
+end

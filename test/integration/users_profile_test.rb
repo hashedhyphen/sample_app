05ea0c7d643cf9e2050257a992fc08bba96fbab6
hashedhyphen <hashedhyphen@gmail.com>
2015-11-20 01:20:15 +0900

    Solve exercises 12.5

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -22,9 +22,16 @@ class UsersShowTest < ActionDispatch::IntegrationTest
     get user_path(@user)
     assert_template 'users/show'
     assert_select 'title', full_title(@user.name)
+    # User's name & logo
     assert_select 'h1', text: @user.name
     assert_select 'h1>img.gravatar'
+    assert_match  @user.following.count.to_s, response.body
+    # Stats
+    assert_select "a[href=?]", following_user_path(@user)
+    assert_match  @user.followers.count.to_s, response.body
+    assert_select "a[href=?]", followers_user_path(@user)
     assert_match  @user.microposts.count.to_s, response.body
+    # Feed
     assert_select 'div.pagination'
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
